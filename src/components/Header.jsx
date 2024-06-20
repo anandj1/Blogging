7456b34cea6ca4f366635c519be6fd33c4813ba5
anandj1 <anandj12215@gmail.com>
2024-06-21 02:43:08 +0530

    New media query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import "../App.css"
 export default function Header() {
-  return (<div className=" w-full fixed top-0 heading ; ">
+  return (<div className=" w-full header fixed top-0 heading ; ">
    <header className=" shadow-xl   text-center " >
    <h1 className=" text-4xl py-5 font-serif uppercase font-bold text-center ">Anand's Blogs</h1>
 
