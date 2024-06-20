3acde57a22a9a7649efa13dd7fe7ee03921432ae
anandj1 <anandj12215@gmail.com>
2024-06-20 23:44:37 +0530

    First commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (<div className=" w-full fixed top-0 background: rgb(251,63,63);
+background: radial-gradient(circle, rgba(251,63,63,1) 7%, rgba(252,70,175,1) 62%); ">
+   <header className=" shadow-xl   text-center " >
+   <h1 className="text-4xl py-5 font-serif uppercase font-bold text-center ">Anand's Blogs</h1>
+
+   </header>
+   </div>
+  );
+}
